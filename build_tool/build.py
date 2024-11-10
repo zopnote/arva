24c6842b1d99216c13a2b25f1af7b6b6fd722973
Zopnote <lennysiebert08@gmail.com>
2024-11-10 17:15:04 +0100

    Rerange plans and build structure

--- build_tool/build.py
+++
@@ -1,9 +0,0 @@
-import argparse
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--mode', type=str, default='release')
-    parser.add_argument('--runtime', action='store_true')
-    parser.add_argument('--')
-    args = parser.parse_args()
-    print(args)
