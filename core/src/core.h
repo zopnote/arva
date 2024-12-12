b27e4e3847e9b8665cbd55b7b0123fc18a1ab3f9
Zopnote <lennysiebert08@gmail.com>
2024-12-12 19:14:53 +0100

    github workflow

---
+++ core/src/core.h
@@ -0,0 +1,14 @@
+// mathlib.h
+#ifndef MATHLIB_H
+#define MATHLIB_H
+
+// Add two numbers
+int add(int a, int b);
+
+// Subtract two numbers
+int subtract(int a, int b);
+
+// Reverse a string (modifies the input string)
+void reverse_string(char *str);
+
+#endif // MATHLIB_H
